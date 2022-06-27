b481376e8dcfec28d0a9cd12aae4e02a143c470a
dammyng <eidetic@outlook.com>
2022-06-26 22:38:53 -0800

    13-nsg - aws

---
+++ terraform/13-nsg/aws.tf
@@ -0,0 +1,21 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_security_group" "nsg" {
+  name_prefix = "nsg-"
+  
+  ingress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
